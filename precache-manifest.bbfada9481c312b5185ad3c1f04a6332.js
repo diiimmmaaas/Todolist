ec5d2dab9256a2708d8312969bcecf09866a2d0d
diiimmmaaas <dm.sokolov1994@gmail.com>
2022-07-25 18:20:21 +0300

    Updates

---
+++ precache-manifest.bbfada9481c312b5185ad3c1f04a6332.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21c39d5dec20d51112cd7cb2ebe9f31",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "31f1ea9756973aae0697",
+    "url": "/Todolist/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "eedc512af3c91e6cc62b",
+    "url": "/Todolist/static/js/2.317bd65b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/Todolist/static/js/2.317bd65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31f1ea9756973aae0697",
+    "url": "/Todolist/static/js/main.d1c228e9.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
